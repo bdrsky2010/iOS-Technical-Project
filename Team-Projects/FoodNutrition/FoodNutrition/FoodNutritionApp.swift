4376ad6ae8f73e03804d433ca5a004d55eb03751
MJKim <qorhaqt@gmail.com>
2023-11-06 21:35:37 +0900

    Add: App의 메인 Scene 부분

---
+++ Team-Projects/FoodNutrition/FoodNutrition/FoodNutritionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodNutritionApp.swift
+//  FoodNutrition
+//
+//  Created by Minjae Kim on 11/2/23.
+//
+
+import SwiftUI
+//
+@main
+struct FoodNutritionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
