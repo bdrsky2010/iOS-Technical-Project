9e6c077b2d59b703c33a62b89a5f2be55548afde
MJKim <qorhaqt@gmail.com>
2023-11-06 21:32:49 +0900

    Add: json 으로부터 디코딩해서 data를 받아오는 부분

---
+++ Team-Projects/FoodNutrition/FoodNutrition/FoodData.swift
@@ -0,0 +1,29 @@
+//
+//  FoodData.swift
+//  FoodNutrition
+//
+//  Created by Minjae Kim on 11/2/23.
+//
+
+import Foundation
+
+var foodData: Food = Food(nutrition: loadJson("foodInfo.json"))
+
+func loadJson<T: Decodable>(_ filename: String) -> T {
+	let data: Data
+	
+	guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+	else { fatalError("\(filename) not found") }
+	
+	do {
+		data = try Data(contentsOf: file)
+	} catch {
+		fatalError("Could not load \(filename): \(error)")
+	}
+	
+	do {
+		return try JSONDecoder().decode(T.self, from: data)
+	} catch {
+		fatalError("Unable to parse \(filename): \(error)")
+	}
+}
