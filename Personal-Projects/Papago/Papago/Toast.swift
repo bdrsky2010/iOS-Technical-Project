902a08995da2d5be61afa0ae9f59f523b656f395
MJKim <qorhaqt@gmail.com>
2023-11-17 00:05:28 +0900

    [Add] 복사 버튼 탭할 시 출력되는 toast 메시지

---
+++ Personal-Projects/Papago/Papago/Toast.swift
@@ -0,0 +1,97 @@
+//
+//  Toast.swift
+//  Papago
+//
+//  Created by Minjae Kim on 11/16/23.
+//
+
+import SwiftUI
+
+struct Toast: ViewModifier {
+	
+	let message: String
+	@Binding var isShowing: Bool
+	let config: Config
+	
+	func body(content: Content) -> some View {
+		ZStack {
+			content
+			toastView
+		}
+	}
+	
+	//	private var toastView: some View {
+	//		VStack {
+	//			Spacer()
+	//			if isShowing {
+	//				HStack {
+	//					Spacer()
+	//					Text(message)
+	//						.font(config.font)
+	//						.foregroundStyle(config.textColor)
+	//					Spacer()
+	//				}
+	//				.background(config.backgroundColor)
+	//				.clipShape(.rect(cornerRadius: 15))
+	//				.onTapGesture {
+	//					isShowing = false
+	//				}
+	//				.onAppear {
+	//					print("나타나라")
+	//					DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+	//						isShowing = false
+	//					}
+	//				}
+	//			}
+	//		}
+	//		.padding(.horizontal, 50)
+	//		.padding(.bottom, 18)
+	//		.animation(.linear(duration: 0.3), value: isShowing)
+	//		.transition(.opacity)
+	//	}
+	private var toastView: some View {
+		VStack {
+			Spacer()
+			if isShowing {
+				HStack {
+					Spacer()
+					Text(message)
+						.foregroundColor(.white)
+						.font(config.font)
+						.padding(8)
+					Spacer()
+				}
+				.background(config.backgroundColor)
+				.clipShape(.rect(cornerRadius: 15))
+				.onTapGesture {
+					isShowing = false
+				}
+				.onAppear {
+					DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+						isShowing = false
+					}
+				}
+			}
+		}
+		.padding(.horizontal, 50)
+		.padding(.bottom, 18)
+		.animation(.linear(duration: 0.3), value: isShowing)
+		.transition(.opacity)
+	}
+	
+	struct Config {
+		let font: Font
+		let textColor: Color
+		let backgroundColor: Color
+	}
+}
+
+extension View {
+	func toast(message: String, isShowing: Binding<Bool>, config: Toast.Config) -> some View {
+		self.modifier(Toast(message: message, isShowing: isShowing, config: config))
+	}
+}
+
+//#Preview {
+//    Toast()
+//}
