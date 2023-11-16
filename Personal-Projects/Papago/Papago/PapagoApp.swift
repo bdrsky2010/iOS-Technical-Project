ac74b3e652033e0f0fe93c2bc05c34565325d503
MJKim <qorhaqt@gmail.com>
2023-11-16 21:02:58 +0900

    [Add] 파파고 클론 코딩

---
+++ Personal-Projects/Papago/Papago/PapagoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PapagoApp.swift
+//  Papago
+//
+//  Created by Minjae Kim on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct PapagoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
