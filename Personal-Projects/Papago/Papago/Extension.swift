d45e3f5c3cc6eb582e204540cbdcfb3e59b7d3b5
MJKim <qorhaqt@gmail.com>
2023-11-16 21:04:57 +0900

    [Add] Color 값을 Hexcode로 사용하기 위한 Extension

---
+++ Personal-Projects/Papago/Papago/Extension.swift
@@ -0,0 +1,23 @@
+//
+//  Extension.swift
+//  Papago
+//
+//  Created by Minjae Kim on 11/14/23.
+//
+
+import SwiftUI
+
+extension Color {
+  init(hex: String) {
+	let scanner = Scanner(string: hex)
+	_ = scanner.scanString("#")
+	
+	var rgb: UInt64 = 0
+	scanner.scanHexInt64(&rgb)
+	
+	let r = Double((rgb >> 16) & 0xFF) / 255.0
+	let g = Double((rgb >>  8) & 0xFF) / 255.0
+	let b = Double((rgb >>  0) & 0xFF) / 255.0
+	self.init(red: r, green: g, blue: b)
+  }
+}
