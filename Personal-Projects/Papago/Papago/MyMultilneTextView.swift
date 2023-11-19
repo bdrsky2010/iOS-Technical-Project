d0d14e5c8990666f8346d181780e3dda3b0af384
MJKim <qorhaqt@gmail.com>
2023-11-19 13:45:02 +0900

    [Modify] custom textview 로직 구현자체는 작동되도록 완료했지만 리팩토링 및 주석 필요

--- Personal-Projects/Papago/Papago/MyMultilneTextView.swift
+++ Personal-Projects/Papago/Papago/MyMultilneTextView.swift
@@ -8,53 +8,142 @@
 import SwiftUI
 
 struct MyMultilneTextView: View {
-    var body: some View {
-		CustomTextfield(text: .constant("안녕하세요"),
-						placeholder: Text("번역할 내용을 입력하세요").foregroundStyle(Color.init(hex: "#8EBBFF")),
-						font: .title1,
-						textColor: Color.init(hex: ""))
-    }
+	@State private var text: String = ""
+	var body: some View {
+		CustomTextfield(isTranslation: .constant(false),
+						text: $text,
+						height: .constant(200),
+						returnKeyboardType: .constant(.done),
+						placeholder: "번역할 내용을 입력하세요",
+						placeholderColor: .lightGray,
+						fontSize: 30,
+						fontWeight: .bold,
+						textColor: .blue,
+						maxHeight: .infinity)
+	}
 }
 
 struct CustomTextfield: UIViewRepresentable {
+	@Binding var isTranslation: Bool
 	@Binding var text: String
-	let placeholder: Text
-	let font: UIFont.TextStyle
-	let textColor: Color
+	@Binding var height: CGFloat
+	@Binding var returnKeyboardType: UIReturnKeyType
+	var placeholder: String?
+	var placeholderColor: UIColor?
+	var fontSize: CGFloat?
+	var fontWeight: UIFont.Weight?
+	var textColor: UIColor?
+	var maxHeight: CGFloat
+	var bacground: UIColor?
 	
-	// 처음에 View 렌더링 해주는 메서드
-	func makeUIView(context: Context) -> some UITextView {
+	func makeCoordinator() -> Coordinator {
+		Coordinator(self)
+	}
+	
+	func makeUIView(context: Context) -> UITextView {
 		let textView = UITextView()
-		textView.font = .preferredFont(forTextStyle: font)
-		textView.autocapitalizationType = .sentences
-		textView.isSelectable = true
 		
+		if let fontSize = fontSize {
+			textView.font = .systemFont(ofSize: fontSize)
+		}
+		if let fontSize = fontSize, let fontWeight = fontWeight {
+			textView.font = .systemFont(ofSize: fontSize, weight: fontWeight)
+		}
+		if let placeholder = placeholder {
+			dump(placeholder)
+			textView.text = placeholder
+		}
+		if let placeholderColor = placeholderColor {
+			textView.textColor = placeholderColor
+		} else {
+			textView.textColor = .lightGray
+		}
+		if let bacground = bacground {
+			textView.backgroundColor = bacground
+		}
+		textView.delegate = context.coordinator
 		return textView
 	}
 	
-	// View 업데이트되면 다시 View 렌더링해주는 메서드
 	func updateUIView(_ uiView: UITextView, context: Context) {
-		uiView.text = text
-		uiView.font = .preferredFont(forTextStyle: font)
+		dump("updateUIView()")
+		if !text.isEmpty {
+			uiView.text = text
+		}
+		if uiView.text == placeholder {
+			uiView.textColor = placeholderColor
+		}
+		uiView.returnKeyType = returnKeyboardType
+		updateHeight(uiView)
 	}
 	
-	func makeCoordinator() -> Coordinator {
-		Coordinator($text)
+	private func updateHeight(_ uiView: UITextView) {
+		dump("updateHeight()")
+		DispatchQueue.main.async {
+			if uiView.contentSize.height <= maxHeight {
+				height = uiView.contentSize.height
+			}
+		}
+	}
+}
+
+class Coordinator: NSObject, UITextViewDelegate {
+	
+	var parent: CustomTextfield
+	
+	init(_ uiTextView: CustomTextfield) {
+		self.parent = uiTextView
 	}
 	
-	class Coordinator: NSObject, UITextViewDelegate {
-		var text: Binding<String>
+	func textViewDidChange(_ textView: UITextView) {
+		dump("textViewDidChange()")
+		self.parent.text = textView.text
 		
-		init(_ text: Binding<String>) {
-			self.text = text
+		if textView.text.isEmpty {
+			textView.text = parent.placeholder
+			textView.textColor = parent.placeholderColor
+		} else {
+			textView.textColor = parent.textColor
 		}
 		
-		func textViewDidChange(_ textView: UITextView) {
-			self.text.wrappedValue = textView.text
+		if textView.text == self.parent.placeholder {
+			textView.text = ""
 		}
+		
+		if textView.text.count > 200 {
+			textView.text.removeLast()
+		}
+	}
+	
+	func textViewDidBeginEditing(_ textView: UITextView) {
+		dump("textViewDidBeginEditing()")
+		if textView.text == self.parent.placeholder {
+			textView.text = ""
+		}
+	}
+	
+	func textViewDidEndEditing(_ textView: UITextView) {
+		dump("textViewDidEndEditing()")
+		if textView.text.isEmpty {
+			textView.text = parent.placeholder
+			textView.textColor = parent.placeholderColor
+			dump(textView.text)
+		}
+	}
+	
+	func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+		if textView.returnKeyType == .done && text == "\n" {
+			parent.text = textView.text
+			parent.isTranslation = true
+			textView.resignFirstResponder()
+		}
+		if textView.returnKeyType == .default {
+			dump("return")
+		}
+		return true
 	}
 }
 
 #Preview {
-    MyMultilneTextView()
+	MyMultilneTextView()
 }
